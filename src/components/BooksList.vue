43bd41030f7e2ae5f66395749a74dc1bff48e871
Vassiliy Yegorov <va@egorovanet.ru>
2023-05-17 08:16:16 +0700

    books init

---
+++ src/components/BooksList.vue
@@ -0,0 +1,13 @@
+<template>
+	<li v-for="(book, id) in books" :key="books.id">
+		{{ id }}: {{ book.title }}
+	</li>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	books: {
+		type: Object,
+	}
+})
+</script>
---
+++ src/components/BooksView.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1>Books</h1>
+	<ul>
+		<BookList :books="books"/>
+	</ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import BookList from './BooksList.vue'
+const books = ref([
+	{ id: 1, title: 'The Wheel of Time' },
+	{ id: 2, title: 'The Way of Kings' },
+	{ id: 3, title: 'Dune' },
+]);
+</script>
